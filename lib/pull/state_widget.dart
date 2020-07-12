0c71da3a5dfb37ff9ec30498d3a3a53730c54017
wangyi <hack888888889@vip.qq.com>
2020-07-12 10:01:33 +0800

    使用multistate控制

--- lib/pull/state_widget.dart
+++ lib/pull/state_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
@@ -5,56 +6,91 @@ import 'package:tuple/tuple.dart';
 
 typedef StateBuilder<M> = Widget Function(BuildContext context, M m);
 
-class MultiStateWidget<P extends SingleProvider<M>, M> extends StatelessWidget {
+class MultiStateWidget<P extends SingleProvider<M>, M> extends StatefulWidget {
   const MultiStateWidget({
     Key key,
     @required this.provider,
     @required this.builder,
+    this.empty,
+    this.error,
+    this.noLogin,
+    this.loading,
   }) : super(key: key);
+
   final P provider;
   final StateBuilder<M> builder;
+  final Widget empty;
+  final Widget error;
+  final Widget noLogin;
+  final Widget loading;
+
+  @override
+  _MultiStateWidgetState<P, M> createState() => _MultiStateWidgetState<P, M>();
+}
+
+class _MultiStateWidgetState<P extends SingleProvider<M>, M>
+    extends State<MultiStateWidget<P, M>> {
+  @override
+  void initState() {
+    widget.provider.getInfo();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.provider.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<P>(
-      create: (_) => provider,
-      child: Selector<P, Tuple3<PageState, M, String>>(
-        builder:
-            (BuildContext context, Tuple3<PageState, M, String> value, __) {
-          final PageState pageState = value.item1;
-          Widget resWidget;
-          if (pageState == PageState.LOADING) {
-            resWidget = b('loading');
-          }
-          if (pageState == PageState.ERROR) {
-            resWidget = b(value?.item3 ?? 'error');
-          }
-          if (pageState == PageState.EMPTY) {
-            resWidget = b(value?.item3 ?? 'empty');
-          }
-          if (pageState == PageState.LOGIN) {
-            resWidget = b(value?.item3 ?? 'no login');
-          }
-          if (pageState == PageState.CONTENT) {
-            resWidget = builder(context, value.item2);
-          }
-
-          return SmartRefresher(
-            controller: provider.refreshController,
-            onRefresh: provider.isRefresh ? provider.onRefresh : null,
-            onLoading: provider.isLoadMore ? provider.onLoadMore : null,
-            header: provider.headerWidget,
-            footer: provider.footWidget,
-            child: resWidget ?? Container(),
-          );
-        },
-        selector: (_, P provider) => Tuple3<PageState, M, String>(
-            provider.pageState, provider.m, provider.msg),
-      ),
+    final Widget selector = Selector<P, Tuple3<PageState, M, String>>(
+      builder: (BuildContext context, Tuple3<PageState, M, String> value, __) {
+        final PageState pageState = value.item1;
+        Widget resWidget;
+        if (pageState == PageState.LOADING) {
+          resWidget = Center(
+              child: widget.loading ?? const CupertinoActivityIndicator());
+        }
+        if (pageState == PageState.ERROR) {
+          resWidget = Center(
+              child: widget.error ?? _messageWidget(value?.item3 ?? 'error'));
+        }
+        if (pageState == PageState.EMPTY) {
+          resWidget = Center(
+              child: widget.empty ?? _messageWidget(value?.item3 ?? 'empty'));
+        }
+        if (pageState == PageState.LOGIN) {
+          resWidget = Center(
+              child:
+                  widget.noLogin ?? _messageWidget(value?.item3 ?? 'no login'));
+        }
+        if (pageState == PageState.CONTENT) {
+          resWidget = widget.builder(context, value.item2);
+        }
+        return SmartRefresher(
+          controller: widget.provider.refreshController,
+          onRefresh:
+              widget.provider.isRefresh ? widget.provider.onRefresh : null,
+          onLoading:
+              widget.provider.isLoadMore ? widget.provider.onLoadMore : null,
+          header: widget.provider.headerWidget,
+          footer: widget.provider.footWidget,
+          enablePullUp: widget.provider.isLoadMore,
+          enablePullDown: widget.provider.isRefresh,
+          child: resWidget ?? Container(),
+        );
+      },
+      selector: (_, P provider) => Tuple3<PageState, M, String>(
+          provider.pageState, provider.m, provider.msg),
+    );
+    return ChangeNotifierProvider<P>.value(
+      value: widget.provider,
+      child: selector,
     );
   }
 
-  Widget b(String s) {
+  Widget _messageWidget(String s) {
     return Text(s);
   }
 }
@@ -70,6 +106,8 @@ abstract class SingleProvider<M> extends ChangeNotifier {
 
   void onLoadMore() {}
 
+  void getInfo() {}
+
   ///empty
   void setEmpty({String msg}) {
     if (_pageState == PageState.EMPTY) {
@@ -80,6 +118,17 @@ abstract class SingleProvider<M> extends ChangeNotifier {
     notifyListeners();
   }
 
+  void onError(dynamic e) {
+    if (m is List && (m as List).isEmpty) {
+      _pageState = PageState.EMPTY;
+      _refreshController.loadFailed();
+      return;
+    }
+    _refreshController.refreshFailed();
+    _refreshController.loadFailed();
+    setError();
+  }
+
   ///error
   void setError({String msg}) {
     if (_pageState == PageState.ERROR) {
@@ -87,6 +136,7 @@ abstract class SingleProvider<M> extends ChangeNotifier {
     }
     _pageState = PageState.ERROR;
     _msg = _msg;
+    resetRefreshController();
     notifyListeners();
   }
 
@@ -110,21 +160,26 @@ abstract class SingleProvider<M> extends ChangeNotifier {
 
   void setContent(M m) {
     _m = m;
-    _pageState = PageState.CONTENT;
+    if (m is List && m.length == 0) {
+      _pageState = PageState.EMPTY;
+    } else {
+      _pageState = PageState.CONTENT;
+    }
     notifyListeners();
   }
 
   void resetRefreshController({bool noData = false}) {
+    refreshController.refreshCompleted();
     if (noData) {
       refreshController.loadNoData();
     } else {
-      refreshController.refreshCompleted();
       refreshController.loadComplete();
     }
   }
 
   @override
   void dispose() {
+    debugPrint("[PROVIDER] dispose!!");
     _refreshController.dispose();
     super.dispose();
   }
@@ -146,6 +201,13 @@ abstract class SingleProvider<M> extends ChangeNotifier {
   Widget get footWidget => const ClassicFooter();
 }
 
+mixin Parame {}
+
+class LoadMoreParame with Parame {
+  int page = 0;
+  int size = 1;
+}
+
 enum PageState {
   EMPTY,
   LOADING,
@@ -157,5 +219,6 @@ enum PageState {
 Widget configRefresh(Widget child) {
   return RefreshConfiguration(
     child: child,
+    hideFooterWhenNotFull: true,
   );
 }
